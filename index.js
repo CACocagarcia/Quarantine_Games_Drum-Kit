5a0184d06a37140b58cc60ee906fa77a0b1a9f73
Carlos Coca <CACocagarciap@outlook.com>
2020-04-30 19:20:18 -0500

    initial file upload

---
+++ index.js
@@ -0,0 +1,142 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", function() { // we dont put handleClick() because otherwise it will call the method immediately. It will call it automatically
+       // alert("I got clcked"); 
+         
+       var buttonInnerHTML = this.innerHTML; 
+        switch(buttonInnerHTML){
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+            break;
+
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+            break;
+
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+            break;
+
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+            break;
+
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+            break;
+            
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+            break;
+
+            case "l":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+            break;
+
+            default:console.log(buttonInnerHTML);
+        }
+
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+// detecting keyboard press
+document.addEventListener("keypress", function(event) { 
+    //console.log(event);
+    makeSound(event.key); 
+    buttonAnimation(event,key);
+});
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+        
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+
+        default:console.log(buttonInnerHTML);
+    }
+
+    buttonAnimation(key);
+}
+
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100); 
+}
+
+/* THIS IS CALLED USING AN ANONYMOUS FUNCTION
+document.querySelector("button").addEventListener("click",function() {  // we dont put handleClick() because otherwise it will call the method immediately. It will call it automatically
+    alert("I got clcked"); 
+});
+*/
+
+/******* CALCULATOR 
+ * function multiply(num1,num2){
+ * return num1*num2;
+ * }
+ * 
+ * function add(num1,num2){
+ * return num1+num2;
+ * }
+ * 
+ * function subtract(num1, num2){
+ * return num1-num2;
+ * }
+ * 
+ * function divide(num1,num2){
+ * return num1/num2;
+ * }
+ * 
+ * function calculator(num1, num2, operator){
+ * return operator (num1, num2);
+ * }
+ * 
+ * calculator(4,5,add); 
+ * 
+ * 
+*/
